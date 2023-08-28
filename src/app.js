468a83642a7333602ff7178ec2acaf0e67b202ef
Theo <theo.piedade@gmail.com>
2023-08-28 04:51:23 -0300

    transactions init

--- src/app.js
+++ src/app.js
@@ -4,12 +4,15 @@ import dotenv from "dotenv";
 import { MongoClient, ObjectId } from "mongodb";
 import joi from 'joi';
 import bcrypt from 'bcrypt';
+import { v4 as uuid } from 'uuid';
+import dayjs from 'dayjs';
 
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 dotenv.config();
+
 
 // DB Connection init
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
@@ -78,9 +81,16 @@ app.post("/signin", async (req, res) => {
 
     const user = await db.collection('users').findOne({ email });
     if(user && bcrypt.compareSync(password, user.password)) {
-
+       const token = uuid();
+       
+       const data = {
+        userId: user._id,
+        token: token,
+       }
+      
        try {
-           return res.sendStatus(200);
+          await db.collection('sessions').insertOne(data);
+          return res.send(token);
         } 
         catch (err) {
            console.log(err);
@@ -101,18 +111,57 @@ app.get('/users', async (req, res) => {
     }
   });
 
-  app.get('/user', async (req, res) => {
-    const {email} = req.body;
+
+  app.post("/transactions", async (req,res) => {
+    const { authorization } = req.header;
+    const token = authorization?.replace('Bearer ', '');
+
+    if(!token) return res.sendStatus(401);
   
+      try {
+       const session = await db.collection("sessions").findOne({ token });
+        if (!session) return res.sendStatus(401);
+  
+          const user = await db.collection("users").findOne({ _id: session.userId })
+  
+          if(user) {
+            const {type, value, description} = req.body;
+            const data = {
+                userID: user,
+                time: dayjs(Date.now()).format('DD/MM'),
+                type: type,
+                value: value,
+                description: description
+            }
+          await db.collection("transactions").insertOne({data});
+          return res.sendStatus(201);
+        } else {
+          res.sendStatus(401);
+        }
+      } catch (err) { 
+          res.status(500).send(err.message)
+      }  
+  });
+
+
+  app.get('/transactions', async (req, res) => {
     try {
-      const user = await db.collection('users').name.findOne({ email });
-      res.send(user);
+      const transactions = await db.collection('transactions').find().toArray();
+      res.send(transactions);
     } catch (error) {
       console.error(error);
       res.sendStatus(500);
     }
   });
 
-
+  app.get('/sessions', async (req, res) => {
+    try {
+      const sessions = await db.collection('sessions').find().toArray();
+      res.send(sessions);
+    } catch (error) {
+      console.error(error);
+      res.sendStatus(500);
+    }
+  });
 
 app.listen(5000);
